19daf37d7a2858ea40f11b61ac2a9832cb64d494
Aryan Sabet Nejad <74782534+aryansabet@users.noreply.github.com>
2021-07-17 17:23:02 +0430

    initial for cmake & clion
    git ignore & start state of project made.

---
+++ cmake-build-debug/CMakeFiles/Hezartoo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hezartoo.dir/main.cpp.obj"
+  "Hezartoo.exe"
+  "Hezartoo.exe.manifest"
+  "Hezartoo.pdb"
+  "libHezartoo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Hezartoo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
